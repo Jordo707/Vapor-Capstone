660bb5952b0014ef7c8ed251001a715b651a5098
Jordan <penttijordo@gmail.com>
2023-10-12 11:46:40 -0400

    added validations to login and signup

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -10,10 +10,16 @@ function Navigation({ isLoaded }){
 	return (
 		<div className='nav-bar'>
 			<span>
-				<div className='vapor-title'>
-					<img className='vapor-logo' src='https://cdn.discordapp.com/attachments/880221705191161867/1159271530044342353/Vapor_Logo_1.png?ex=65306abe&is=651df5be&hm=34a5f13a1414a99f989c99f610e9f417a23391d416b1ad9d715cbe57714b9063&'></img>
-					<h2>VAPOR</h2>
-				</div>
+				<NavLink to="/">
+					<div className='vapor-title'>
+						<img
+							className='vapor-logo'
+							src='https://cdn.discordapp.com/attachments/880221705191161867/1159271530044342353/Vapor_Logo_1.png?ex=65306abe&is=651df5be&hm=34a5f13a1414a99f989c99f610e9f417a23391d416b1ad9d715cbe57714b9063&'
+							alt="Vapor Logo"
+						/>
+						<h2>VAPOR</h2>
+					</div>
+				</NavLink>
 			</span>
 			<span>
 				<NavLink exact to="/store">Store</NavLink>
