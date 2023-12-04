2d0cfbfc013d0bb78ee10d0447cf857ab0daa6a8
Jordan <penttijordo@gmail.com>
2023-12-03 19:22:14 -0500

    tweak seed data

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -8,28 +8,21 @@ def seed_reviews():
         user_id=3,
         review_text='Unrealistic premise. People can work more than 60 hours a week without issue. Refunded.'
     )
-    burnout_review_2 = Review(
-        recomended=True,
-        game_id=1,
-        user_id=1,
-        review_text='This game really speaks to me. 10/10, would play again.'
-    )
     africanus_review_1 = Review(
-        recomended=True,
-        game_id=2,
-        user_id=1,
-        review_text='Africa really does have a rich and fascinating history and this game does an incredible job of bringing it to life.'
-    )
-    africanus_review_2 = Review(
         recomended=True,
         game_id=2,
         user_id=2,
         review_text='The gameplay is solid, if not spectacular, but the setting itself really sets it apart from the other grand strategy games out there.'
     )
+    spacefield_review_1 = Review(
+        recomended=True,
+        game_id=3,
+        user_id=1,
+        review_text="It wasn't the industry-defining standard we were hoping for, but it was certainly fun."
+    )
     db.session.add(burnout_review_1)
-    db.session.add(burnout_review_2)
     db.session.add(africanus_review_1)
-    db.session.add(africanus_review_2)
+    db.session.add(spacefield_review_1)
     db.session.commit()
 
 def undo_reviews():
